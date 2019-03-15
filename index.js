e5455f3ccba5d46f032aef4a94947dadf7d8b138
Julian Otten <j.otten03@student.alfa-college.nl>
2019-03-15 16:11:41 +0100

    added comments to code and some minor tiny changes

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+// Require packages
 const electron = require("electron");
 const url = require("url");
 const path = require("path");
-
+// Pull out needed objects
 const { app, BrowserWindow, Menu, ipcMain} = electron;
 
-// process.env.NODE_ENV = 'production';
+// Have the application run in production
+process.env.NODE_ENV = 'production';
 
+// Define application windows (filled later)
 let mainWindow;
 let addWindow;
 
+// Once the application is ready
 app.on("ready", () => {
+  // Create a new window
   mainWindow = new BrowserWindow({});
+  // Load the mainWindow.html into the mainWindow 
   mainWindow.loadURL(
     url.format({
       pathname: path.join(__dirname, "mainWindow.html"),
@@ -19,21 +25,30 @@ app.on("ready", () => {
     })
   );
 
+  // If someone closes mainWindow, close all other windows
   mainWindow.on("closed", () => {
     app.quit();
   });
 
   // Build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Set the top menu 
   Menu.setApplicationMenu(mainMenu);
 });
 
+/**
+ * Ceate the add item window
+ * 
+ * @return void
+ */
 function createAddWindow() {
+  // Deifne window settings
   addWindow = new BrowserWindow({
     width: 300,
     height: 200,
     title: "Add Shopping List Item"
   });
+  // Load the addWindow html into the window
   addWindow.loadURL(
     url.format({
       pathname: path.join(__dirname, "addWindow.html"),
@@ -42,13 +57,15 @@ function createAddWindow() {
     })
   );
 
+  // Once the addWindow is closed, clean out the variable, "garbage collection"
   addWindow.on("close", () => {
     addWindow = null;
   });
 }
 
-// Catch item:add
+// Catch item:add from addWindow
 ipcMain.on('item:add', (event, item) => {
+  // Send item to mainWindow (I have no clue why it isnt send there directly)
   mainWindow.webContents.send('item:add', item);
   addWindow.close();
 });
@@ -56,10 +73,14 @@ ipcMain.on('item:add', (event, item) => {
 // Menu template
 const mainMenuTemplate = [
   {
+    // Name of the menu item
     label: "File",
+    // Submenu for File
     submenu: [
       {
+        // name of the submenu item
         label: "Add Item",
+        // the function that gets called once you click on the item
         click() {
           createAddWindow();
         }
@@ -72,8 +93,10 @@ const mainMenuTemplate = [
       },
       {
         label: "Quit",
+        // Key shortcuts. process.platform == "darwin" is a macOs check. if Os == mac use Command+Q, else use Ctrl+Q. shortcuts also run the click function
         accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
         click() {
+          // quit the app
           app.quit();
         }
       }
@@ -81,8 +104,12 @@ const mainMenuTemplate = [
   }
 ];
 
+/* Mac has a weird bug where it doesnt display the first menu item properly. 
+For mac you add an empty menu item.
+any other platform shouldnt have this empty menu item as it will look weird */
 if (process.platform == "darwin") mainMenuTemplate.unshift({});
 
+// If the application is not running in production, add the option to open Devtools and a reload
 if(process.env.NODE_ENV !== 'production') {
   mainMenuTemplate.push({
     label: 'Developer Tools',
@@ -91,10 +118,12 @@ if(process.env.NODE_ENV !== 'production') {
         label:'Toggle DevTools',
         accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
         click(item, focusedWindow) {
+          // toggle the devtools on or off in the focused window
           focusedWindow.toggleDevTools();
         }
       },
       {
+        // Adds reload functionality. this already excists within electron
         role: 'reload'
       }
     ]
